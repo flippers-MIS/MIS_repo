e0596e8e449a2ce9bd8f03e99caf32a6fc107346
Felix Schauer <felix.paul.schauer@gmail.com>
2025-01-27 15:31:26 +0100

    Finished Printer class in MIS_Database

--- MIS_Backend/MIS_Database/Printer.cs
+++ MIS_Backend/MIS_Database/Printer.cs
@@ -1,40 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MIS_Database;
 
-namespace MIS_Database
+public class Printer
 {
-    public class Printer
+    public required int Id { get; init; }
+    public required string Name { get; set; }
+    public required double FormatWidth { get; set; }
+    public required double FormatLength { get; set; }
+    public string FormatWidthXLength => $"{FormatWidth}x{FormatLength}";
+    public required string FormatName { get; set; }
+    public required double BuyPrinceFor4cKlick { get; set; }        //einkaufspreis pro farbigen klick
+    public required double BuyPrinceFor1cKlick { get; set; }     //einkaufspreis pro schwarzweiss klick
+    public required double SellPriceFor4cKlick { get; set; }       //preis pro farbigen klick
+    public required double SellPriceFor1cKlick { get; set; }       //preis pro schwarzweiss klick
+    public required double UnprintedEdgeMM { get; set; }
+
+    private Printer() { }
+
+    public static Printer Parse(string line)
     {
-        public required int Id { get; init; }
-        public required string Name { get; set; }
-        public required double FormatWidth { get; set; }
-        public required double FormatLength { get; set; }
-        public string FormatWidthXLength => $"{FormatWidth}x{FormatLength}";
-        public required string FormatName { get; set; }
-
-        private Printer() { }
-
-        public static Printer Parse(string line)
+        string[] parts;
+        try
         {
-            ArgumentNullException.ThrowIfNull(line);
-
-            string[] parts = line.Split(',');
-
-            //id,name,format_width,format_length,format_name
-            //1,Printer444,78x22,A2
-
-
-            return new Printer
-            {
-                Id = int.Parse(parts[0]),
-                Name = parts[1],
-                FormatWidth = double.Parse(parts[2]),
-                FormatLength = double.Parse(parts[3]),
-                FormatName = parts[3],
-            };
+          parts = line.Split(',');
         }
+        catch (Exception e)
+        {
+            throw new Exception($"Error parsing line: {line}", e);
+        }
+
+        //id,  name,  format_width,format_length,format_name, buyPrinceFor4cKlick, buyPrinceFor1cKlick, sellPriceFor4cKlick, sellPriceFor1cKlick, unprintedEdgeMM
+        //1, Printer444, 78.0,            22.0,        A2,          0.002,                0.001,                 0.1,              0.05,                  5.0
+
+        return new Printer
+        {
+            Id = int.Parse(parts[0]),
+            Name = parts[1],
+            FormatWidth = double.Parse(parts[2]),
+            FormatLength = double.Parse(parts[3]),
+            FormatName = parts[3],
+            BuyPrinceFor4cKlick = double.Parse(parts[4]),
+            BuyPrinceFor1cKlick = double.Parse(parts[5]),
+            SellPriceFor4cKlick = double.Parse(parts[6]),
+            SellPriceFor1cKlick = double.Parse(parts[7]),
+            UnprintedEdgeMM = double.Parse(parts[8]),
+        };
     }
 }
