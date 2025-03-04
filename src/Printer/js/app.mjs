ed7bbe56e6c519b1e79afb4dfcfc10efed3821fc
AdelsgruberN <AdelsgruberN190183@sus.htl-grieskirchen.at>
2025-03-04 10:49:07 +0100

    #1 Updated Add-Button with correct data & inplemented user messages

--- src/Printer/js/app.mjs
+++ src/Printer/js/app.mjs
@@ -1,43 +1,50 @@
 import * as Helpers from '../../HelperFunctions/helper.mjs';
 
-const path = 'http://10.0.3.10:32769/Printer';
+const path = 'http://localhost:5192/Printer';
 
 const printers = [];
 let updatePrinterId = 0;
 
+
 const table = document.querySelector('#printer-list tbody');
+
+
+const description = document.querySelector('#description');
+const border = document.querySelector('#margin');
 const name = document.querySelector('#name');
+const click4cEK = document.querySelector('#klick-4c-ek');
+const click1cEK = document.querySelector('#klick-1c-ek');
+const click4cVK = document.querySelector('#klick-4c-vk');
+const click1cVK = document.querySelector('#klick-1c-vk');
 const formatL = document.querySelector('#length');
 const formatB = document.querySelector('#width');
-const border = document.querySelector('#margin');
-// const click4cEK = document.querySelector('#klick-4c-ek');
-// const click1cEK = document.querySelector('#klick-1c-ek');
-// const click4cVK = document.querySelector('#klick-4c-vk');
-// const click1cVK = document.querySelector('#klick-1c-vk');
-
-const updateButton = document.querySelector("#button-update");
+const inactive = document.querySelector('#inactive');
 
 const inputs = document.querySelectorAll('input[type="text"]');
+
+
+
+const userMessage = document.querySelector("#userMessage")
+
+const updateButton = document.querySelector("#button-update");
 const addbutton = document.getElementById('button-add');
 const clearbutton = document.getElementById('button-clear');
-
-// const apiUrl = "http://localhost:5192/Printer"; 
-
 
 
 
 
 function initializeTable(printerJson) {
+    console.log("printerJson: ", JSON.stringify(printerJson));
+
     printerJson.forEach(item => 
     {
         printers.push(item);
         const row = document.createElement('tr');
         row.innerHTML = `
-        <td>${item.maschinenName}</td>
-        <td>${item.maschinenFormatL}</td>
-        <td>${item.maschinenFormatB}</td>
+        <td>${item.name}</td>
+        <td>${item.druckformatL}</td>
+        <td>${item.druckformatB}</td>
         `;
-        row.setAttribute('data-id', item.printerId);
         row.addEventListener('click', (e) => {
             const printer = printers.find(item => item.printerId === parseInt(e.currentTarget.getAttribute('data-id')));
             updatePrinterId = parseInt(e.currentTarget.getAttribute('data-id'));
@@ -61,18 +68,25 @@ function initializeTable(printerJson) {
 
 async function addRow() 
 {
+    const printer = [];
   const newPrinter = 
   {
-      PrinterId: parseInt(printers.length),
-      MaschinenName: name.value,
-      MaschinenFormatL: parseInt(formatL.value),
-      MaschinenFormatB: parseInt(formatB.value),
-      FarbFormat: '0',
-      UnbedruckbarerRand: parseInt(border.value),
+      Bezeichnung: description.value,
+      Rand: parseInt(border.value),
+      Name: name.value,
+      EK4c: click4cEK.value,
+      EK1c: click1cEK.value,
+      VK4c: click4cVK.value,
+      VK1c: click1cVK.value,
+      DruckformatL: parseInt(formatL.value),
+      DruckformatB: parseInt(formatB.value),
+      Inaktiv: inactive ? 1 : 0,
   };
   console.log(JSON.stringify(newPrinter));
+  addbutton.disabled = true;
   try 
   {
+    console.log('sending request');
     const response = await fetch(path, 
     {
         method: "POST",
@@ -80,14 +94,18 @@ async function addRow()
         body: JSON.stringify(newPrinter),
     });
 
+    const data = await response.json();
+    console.log('got request: ', data);
+
     if (!response.ok) 
     {
         throw new Error(`Fehler beim Hinzufügen: ${response.status}`);
     }
 
-    alert("Drucker erfolgreich hinzugefügt!");
-    location.reload(); 
-
+    printer.push(data);
+    initializeTable(printer); 
+    userMessage.textContent = "Drucker wurde erfolgreich hinzugefügt";
+    userMessage.style.color = "green";
   } 
   catch (error) 
   {
@@ -95,62 +113,6 @@ async function addRow()
   }
   Array.from(inputs).some(input => input.value = "");
 }
-
-
-
-
-/*------------------------------------------DELETE---------------------------------------------------*/
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-    const table = document.querySelector("#printer-list tbody");
-    const contextMenu = document.createElement("div");
-    contextMenu.classList.add("context-menu");
-    contextMenu.innerHTML = '<button id="context-delete">Löschen</button>';
-    document.body.appendChild(contextMenu);
-
-    let selectedPrinterId = null;
-
-    table.addEventListener("contextmenu", (event) => 
-    {
-        event.preventDefault();
-        const row = event.target.closest("tr");
-        if (!row) return;
-
-        selectedPrinterId = row.getAttribute("data-id");
-        contextMenu.style.top = `${event.pageY}px`;
-        contextMenu.style.left = `${event.pageX}px`;
-        contextMenu.style.display = "block";
-    });
-
-    document.addEventListener("click", () => {
-        contextMenu.style.display = "none";
-    });
-
-    document.getElementById("context-delete").addEventListener("click", async () => {
-        if (selectedPrinterId) 
-            {
-            try 
-            {
-                const response = await fetch(`${path}/${selectedPrinterId}`, 
-                {
-                    method: "DELETE",
-                });
-
-                if (!response.ok) {
-                    throw new Error(`Fehler beim Löschen: ${response.status}`);
-                }
-
-                alert("Drucker erfolgreich gelöscht!");
-                location.reload();
-            } catch (error) {
-                console.error("Fehler beim Löschen des Druckers: ", error);
-            }
-        }
-    });
-});
-
 
 
 
@@ -212,6 +174,7 @@ updateButton.addEventListener('click', async () => {
 
 clearbutton.addEventListener('click', () => {
     Array.from(inputs).some(input => input.value = "");
+    clearbutton.disabled = true;
 });
 
 
@@ -253,49 +216,10 @@ document.querySelectorAll('input[type="text"]').forEach(input =>
 
 addbutton.addEventListener('click', async () => {
     addRow();
-    // const description = document.getElementById("description").value.trim();
-    // const margin = document.getElementById("margin").value.trim();
-    // const name = document.getElementById("name").value.trim();
-    // const klick_4c_ek = document.getElementById("klick-4c-ek").value.trim();
-    // const klick_1c_ek = document.getElementById("klick-1c-ek").value.trim();
-    // const klick_4c_vk = document.getElementById("klick-4c-vk").value.trim();
-    // const klick_1c_vk = document.getElementById("klick-1c-vk").value.trim();
-
-
-    // const newPrinter = {
-    //     Description: description,
-    //     Margin: parseInt(margin),
-    //     Name: name,
-    //     Klick_4c_ek: parseInt(klick_4c_ek),
-    //     Klick_1c_ek: parseInt(klick_1c_ek),
-    //     Klick_4c_vk: parseInt(klick_4c_vk),
-    //     Klick_1c_vk: parseInt(klick_1c_vk)
-    // };
-
-    // try 
-    // {
-    //     const response = await fetch(apiUrl, 
-    //     {
-    //         method: "POST",
-    //         headers: { "Content-Type": "application/json", },
-    //         body: JSON.stringify(newPrinter)
-    //     });
-            
-    //     alert("Drucker erfolgreich hinzugefügt!");
-    //     //populateTable(); // Tabelle aktualisieren
-
-    // } 
-    // catch (error) 
-    // {
-    //     alert("Drucker konnte nicht hinzugefügt werden!");
-    //     console.error("Fehler beim Hinzufügen eines neuen Druckers: ", error);
-    // }
-
-    // Array.from(inputs).some(input => input.value = "");
 });
 
 
 
 //Main
-// let printerJson = await Helpers.fetchTable(path);
-// initializeTable(printerJson);
+let printerJson = await Helpers.fetchTable(path);
+initializeTable(printerJson);
